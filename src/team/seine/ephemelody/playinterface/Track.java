205fe4e0e24347b71a54837abee5c7e681d49ff3
a884322916 <70182637+a884322916@users.noreply.github.com>
2021-05-30 22:16:42 +0800

    修改

--- src/team/seine/ephemelody/playinterface/Track.java
+++ src/team/seine/ephemelody/playinterface/Track.java
@@ -53,6 +53,7 @@ public abstract class Track extends Thread {// The track of the note.
             }*/
             // paintnotes
             // painttracks
+            //...
         }
     }
 }
