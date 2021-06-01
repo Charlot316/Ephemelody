1a8a3605a11a02c2853830fdead06bb6179433b9
a884322916 <70182637+a884322916@users.noreply.github.com>
2021-06-01 19:26:50 +0800

    修改6.1

--- src/team/seine/ephemelody/scenes/Home.java
+++ src/team/seine/ephemelody/scenes/Home.java
@@ -20,20 +20,22 @@ public class Home extends JPanel{
         setLayout(null);
         ratingButton = Load.image("home/潜力值_6.png");
         setupButton = Load.image("home/设置.png");
-        playButton = Load.image("home/开始游戏.png");
+        playButton = Load.image("home/start.png");
         homeButton = Load.image("home/主页.png");
         easyButton = Load.image("home/简单.png");
         normalButton = Load.image("home/普通.png");
         difficultButton = Load.image("home/困难.png");
-        song1 = Load.image("home/歌曲1.png");
+        song1 = Load.image("home/song1.png");
         setBackground(null);
         setOpaque(false);
     }
 
     public void paint(Graphics g) {
         g.drawImage(song1, Data.WIDTH / 2, 80, null);
-        g.drawImage(easyButton, Data.WIDTH / 2 - 160, 500, null);
-        g.drawImage(normalButton, Data.WIDTH / 2 + 160, 500, null);
-        g.drawImage(difficultButton, Data.WIDTH / 2 + 480, 500, null);
+        g.drawImage(easyButton, Data.WIDTH / 2 - 140, 580, null);
+        g.drawImage(normalButton, Data.WIDTH / 2 + 140, 580, null);
+        g.drawImage(difficultButton, Data.WIDTH / 2 + 420, 580, null);
+        g.drawImage(playButton, Data.WIDTH / 2, 750, null);
+
     }
 }
