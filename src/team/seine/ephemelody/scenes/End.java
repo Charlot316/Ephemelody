7ff9ee79d40d80ba607d47b76f76c29127c55715
a884322916 <70182637+a884322916@users.noreply.github.com>
2021-06-04 15:31:38 +0800

    结算页面初步

--- src/team/seine/ephemelody/scenes/End.java
+++ src/team/seine/ephemelody/scenes/End.java
@@ -14,21 +14,30 @@ public class End extends JPanel implements Scenes, MouseMotionListener, MouseLis
     public Image menuImg;
     public Image endBackgroundImg;
     public Image nowSongImg;
-
+    public Image listImg;
+    public Image myGradeImg;
+    public Image gradeImg;
     public End() {
         setBounds(0, 0, Data.WIDTH, Data.HEIGHT);
         setVisible(true);
         setOpaque(false);
         endBackgroundImg = Load.image("end/结算背景.png");
-//        nowSongImg =
+        nowSongImg = Load.image("song/1/song1.png"); // 到时候得改成具体的歌曲背景
+        listImg = Load.image("end/排行榜.png");
+        myGradeImg = Load.image("end/我的成绩.png");
+        gradeImg = Load.image("end/成绩条.png");
+
         addMouseListener(this);
         addMouseMotionListener(this);
     }
     public void paint(Graphics g) {
         g.drawImage(endBackgroundImg, 0, (Data.HEIGHT - 632) / 2, null);
-
+        g.drawImage(nowSongImg, 90, 369, null);
+        g.drawImage(myGradeImg, 993, 680, null);
+//        g.drawImage(gradeImg, 720, 350, null);
         g.setFont(new Font("黑体", Font.PLAIN, 80));
         g.setColor(new Color(119, 97, 125));
+
     }
 
     @Override
