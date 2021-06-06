4de3a3fb255ce62b4651469aebed309f9a4fe193
a884322916 <70182637+a884322916@users.noreply.github.com>
2021-06-06 23:50:39 +0800

    添加歌曲指示条（未完成）

--- src/team/seine/ephemelody/data/Data.java
+++ src/team/seine/ephemelody/data/Data.java
@@ -45,8 +45,20 @@ public class Data {
         };
         backgroundImg = Load.image("home/背景.png");
         startButton = Load.image("home/开始游戏.png");*/
-        //songList = Arrays.asList("最炫民族风", "你是我的小呀小苹果", "怎么爱你都不嫌多", "我到底在干啥", "这就是最后一首歌");
+        songList = Arrays.asList("第一首歌", "第二首歌", "第三首歌", "第四首歌", "第五首歌");
+    }
 
+    public void changeSongList(int way, int chooseSong) {
+        if (way == 1) {
+            String song = songList.get(0);
+            for (int i = 0; i < 4; i++) {
+                
+            }
+        } else if (way == 2) {
+
+        } else if (way == 3) {
+
+        }
     }
 
 }
