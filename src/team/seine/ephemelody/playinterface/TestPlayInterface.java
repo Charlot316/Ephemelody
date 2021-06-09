b78c20e0beaca9a1cb0491ede73c535d1148aca7
Charlot0316 <2280583075@qq.com>
2021-06-09 18:49:25 +0800

    修改第二首谱面

--- src/team/seine/ephemelody/playinterface/TestPlayInterface.java
+++ src/team/seine/ephemelody/playinterface/TestPlayInterface.java
@@ -3,19 +3,22 @@ package team.seine.ephemelody.playinterface;
 import team.seine.ephemelody.scenes.PlayInterface;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Scanner;
 
 public class TestPlayInterface {
     public static JFrame Frame;
 
     public static void main(String[] args) {
-        Frame = new JFrame("游玩界面");
-        Frame.setSize(200, 200);
-        Frame.setVisible(true);
-        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        PlayInterface playInterface = new PlayInterface(1, 1);
-        playInterface.setOpaque(true);
-        Frame.add(playInterface);
-        Frame.setContentPane(playInterface);
+        Scanner input = new Scanner(System.in);
+        while(true){
+            String s=input.nextLine();
+            String[] arguments = s.split("\\s+");
+            int i=0+(int)(Math.random()*256);
+            int j=0+(int)(Math.random()*256);
+            int k=0+(int)(Math.random()*256);
+            if(s!=""){
+                System.out.println(arguments[0]+"\t"+"3"+"\t"+arguments[3]+"\t"+arguments[3]+"\t"+i+"\t"+j+"\t"+k);
+            }
+        }
     }
 }
