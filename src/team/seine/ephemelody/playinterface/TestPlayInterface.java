7be9e5062f2474006eec41a036004388c067d62d
Charlot0316 <2280583075@qq.com>
2021-06-11 01:43:48 +0800

    歌曲三困难谱面

--- src/team/seine/ephemelody/playinterface/TestPlayInterface.java
+++ src/team/seine/ephemelody/playinterface/TestPlayInterface.java
@@ -10,14 +10,20 @@ public class TestPlayInterface {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int i=0;
         while(true){
             String s=input.nextLine();
             String[] arguments = s.split("\\s+");
-            int i=0+(int)(Math.random()*256);
-            int j=0+(int)(Math.random()*256);
-            int k=0+(int)(Math.random()*256);
+//            int i=0+(int)(Math.random()*256);
+//            int j=0+(int)(Math.random()*256);
+//            int k=0+(int)(Math.random()*256);
+
             if(s!=""){
-                System.out.println(arguments[0]+"\t"+"3"+"\t"+arguments[3]+"\t"+arguments[3]+"\t"+i+"\t"+j+"\t"+k);
+                double zuo,you;
+                if (i%2==0) {zuo=0.8;you=0.2;}
+                else {zuo=0.2;you=0.8;}
+                i++;
+                System.out.println("5\t"+"1"+"\t"+arguments[0]+"\t"+arguments[1]+"\t"+zuo+"\t"+you+"\t");
             }
         }
     }
