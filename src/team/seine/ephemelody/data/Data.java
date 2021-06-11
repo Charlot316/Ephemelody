78b29f6b783d2efffb98f12a43f41ed7fd01e38e
Charlot0316 <2280583075@qq.com>
2021-06-12 03:22:59 +0800

    更改难度

--- src/team/seine/ephemelody/data/Data.java
+++ src/team/seine/ephemelody/data/Data.java
@@ -48,11 +48,11 @@ public class Data {
         isFirstLogin = true;
         songList = Arrays.asList(new Song(), new Song(), new Song(), new Song(), new Song());
         realSongList = Arrays.asList(
-                new Song(0, "新手指导", 1, 2, 3),
-                new Song(1, "熱愛発覚中", 2, 5, 9),
-                new Song(2, " world.excute(me);", 3, 6, 10),
-                new Song(3, "迷える音色は恋の歌", 3, 6, 11),
-                new Song(4, "Aegleseeker", 3, 6, 11)
+                new Song(0, "新手指导", 0, 0, 1),
+                new Song(1, "熱愛発覚中", 2, 8, 10),
+                new Song(2, " world.excute(me);", 5, 6, 9),
+                new Song(3, "迷える音色は恋の歌", 4, 7, 11),
+                new Song(4, "Aegleseeker", 3, 4, 11)
         );
 //        for(int i=0;i<realSongList.size();i++){
 //            Songs[i]=Load.sound(String.valueOf(realSongList.get(i).getSongID()));
